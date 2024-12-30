518a4ce7a2eff839540e378f0378bd929ddd1c33
tom.piszczecki <tom.piszczecki@gilmond.co.uk>
2024-12-30 06:56:35 +0000

    remove commented code

--- src/components/CustomNumberInput.tsx
+++ src/components/CustomNumberInput.tsx
@@ -20,35 +20,35 @@ const CustomNumberInput = <T extends FieldValues>({ labelText, control, name, re
         return (
 
 
-            <Controller
-                control={control}
-                name={name}
-                render={({ field: { value, onChange } }) => (
-                    <TextField
-                        type="number"
-                        onChange={(e) => onChange(stringToNumber(e.target.value))}
-                    />
-                )}
-            />
-            // <div className="container">
-            //     <FormFieldLabel labelText={labelText} required={required} />
-            //     <Controller
-            //         control={control}
-            //         name={name}
-            //         render={({ field: { value, onChange } }) => (
-            //             <Box className="spinner-container">
-            //                 <Box className="value-container">
-            //                     <span className="custom-input-number">{value}</span>
-            //                     <span className="percentage">%</span>
-            //                 </Box>
-            //                 <Box className="spinners">
-            //                     <button className="spinner-button-up" onClick={onChange}><ArrowDropUpIcon className="spinner-icon" /></button>
-            //                     <button className="spinner-button-down" onClick={onChange}><ArrowDropDownIcon className="spinner-icon" /></button>
-            //                 </Box>
-            //             </Box>
-            //         )}
-            //     />
-            // </div>
+            // <Controller
+            //     control={control}
+            //     name={name}
+            //     render={({ field: { value, onChange } }) => (
+            //         <TextField
+            //             type="number"
+            //             onChange={(e) => onChange(stringToNumber(e.target.value))}
+            //         />
+            //     )}
+            // />
+            <div className="container">
+                <FormFieldLabel labelText={labelText} required={required} />
+                <Controller
+                    control={control}
+                    name={name}
+                    render={({ field: { value, onChange } }) => (
+                        <Box className="spinner-container">
+                            <Box className="value-container">
+                                <span className="custom-input-number">{value}</span>
+                                <span className="percentage">%</span>
+                            </Box>
+                            <Box className="spinners">
+                                <button className="spinner-button-up" onClick={onChange}><ArrowDropUpIcon className="spinner-icon" /></button>
+                                <button className="spinner-button-down" onClick={onChange}><ArrowDropDownIcon className="spinner-icon" /></button>
+                            </Box>
+                        </Box>
+                    )}
+                />
+            </div>
         );
     }
     return <></>;
